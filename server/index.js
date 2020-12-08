007b3852adb106d9c929f0789d0b587a5545cc0a
Matteo Besio <matteo.besio@gmail.com>
2020-12-07 21:09:07 -0800

    trying to serve the static files from the server

--- server/index.js
+++ server/index.js
@@ -2,10 +2,31 @@ const express = require('express');
 const app = express();
 const port = 3000;
 var cors = require('cors');
+const axios = require('axios');
 
 app.use(cors());
 app.use(express.static('client/dist'));
 
+app.get('/:id', (req, res) => {
+  var isbn = req.params.id;
+  console.log('req.params.id ', req.params.id);
+
+  // trying to hit the service endpoint to serve the static assets
+  axios({
+    method: 'get',
+    url: 'http://localhost:3001'
+  })
+    .then((data) => {
+      res.send('test  ');
+    })
+
+
+  res.send(`'Looking to render the book with isbn: ${req.params.id}`);
+
+});
+
+
 app.listen(port, () => {
+
   console.log(`Listening on port ${port}`);
 })
