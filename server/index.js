534a41c509b46416a8c78c08e8722df5b66c330d
Matteo Besio <matteo.besio@gmail.com>
2020-12-07 19:24:15 -0800

    cleaned up the files

--- server/index.js
+++ server/index.js
@@ -3,19 +3,9 @@ const app = express();
 const port = 3000;
 var cors = require('cors');
 
-// var httpProxy = require('http-proxy');
-// var proxy = httpProxy.createProxyServer();
-// const { createProxyMiddleware } = require('http-proxy-middleware');
-
 app.use(cors());
-// var itemSelection = 'http://localhost:3001';
-// var product = 'http://localhost:5001';
-// var reviews = 'http://localhost:8000';
-// var carousel = 'http://localhost:3004';
-
 app.use(express.static('client/dist'));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 })
-
